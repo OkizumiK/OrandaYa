b8cd8ccfdcfe630ff7c45426c304ae62c2db1b64
hayashi.yuki <hayashi.yuki.t6m@jp.nssol.nipponsteel.com>
2022-07-08 15:27:33 +0900

    add:別の開発環境

---
+++ client/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js Appssss"/>
+
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+// import sample from './assets/employee.json'
+import recommend_Headers from './assets/Recommend_Header.json'
+
+export default {
+  name: 'App',
+  data () {
+      return {
+        msg: 'Welcome to Your Vue.js App',
+        recommend_Headers: recommend_Headers
+      }
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+{{ recommend_Headers }}
+  </div>
+</template>
+
+<script>
+import recommend_Headers from '../assets/Recommend_Header.json'
+export default {
+  name: 'HelloWorld',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App',
+        test: 'test',
+        recommend_Headers: recommend_Headers
+      }
+    },
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
