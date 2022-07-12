a34d6600ac941b1551a7f643566d8f3528ce852f
hayashi.yuki <hayashi.yuki.t6m@jp.nssol.nipponsteel.com>
2022-07-12 15:12:11 +0900

    add and update: routerとりあえず動くように

---
+++ client/src/views/EmployeeList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+  <h2>{{ msg }}</h2>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EmployeeList',
+    data () {
+      return {
+        msg: 'トップページ'
+      }
+    },
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
